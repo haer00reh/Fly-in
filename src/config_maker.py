f235e1448a9e788d2d27de92cfc84190e15e8675
haer00reh <haer-reh@student.1337.ma>
2026-07-22 06:28:56 +0100

    progress

--- src/config_maker.py
+++ src/config_maker.py
@@ -1,8 +1,43 @@
 from pydantic import BaseModel
 
+
+class hub(BaseModel):
+    name: str
+    x: int
+    y: int
+    color: str
+    zone_type: str
+    max_drones: int
+
+
+class connection(BaseModel):
+    hub1: hub
+    hub2: hub
+    max_link: int
+
+
+class end_hub(hub):
+    pass
+
+
+class start_hub(hub):
+    pass
+
+
+class drone(BaseModel):
+    id: int
+    start_hub: hub
+    end_hub: hub
+
+
+
 class Config(BaseModel):
     nb_drones: int
-    start_hub: str
-    end_hub: str
-    hubs: list[str]
-    connections: list[tuple[str, str]]
+    start: start_hub
+    end: end_hub
+    hubs: list[hub]
+    connections: list[connection]
+    config_table: list[str] = []
+    def __init__(self):
+        pass
+
